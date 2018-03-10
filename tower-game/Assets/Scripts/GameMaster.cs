2f1bf9fb9080e6ed17db4709faf68a1e0d4979e4
Stanley Liu <liustan24@gmail.com>
2018-03-10 12:37:02 +0800

    Added pathfinding files

--- tower-game/Assets/Scripts/GameMaster.cs
+++ tower-game/Assets/Scripts/GameMaster.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
-	public void KillPlayer (Player player) {
+	public static void KillPlayer (Player player) {
 		Destroy (player.gameObject);
 	}
 }
