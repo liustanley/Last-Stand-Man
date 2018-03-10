50427f5400b1e683fb82bf23987a7c213719e94b
Jonathan Serbent <jonathanserbent@gmail.com>
2018-03-10 21:59:50 +0800

    Main menu v1.0
    Still needs some info, and for settings to be complete, as well as credits

---
+++ tower-game/Assets/Scripts/Fader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour {
+
+	public Texture2D fadeImage;
+	public float fadeSpeed = 1.0f;
+
+	private int drawDepth = -1000;
+	private int fadeDir = -1;
+	private float alpha = 1.0f;
+
+	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	void OnGUI()
+	{
+		alpha += (fadeDir * Time.deltaTime) / fadeSpeed;
+
+		alpha = Mathf.Clamp01 (alpha);
+
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height)  ,fadeImage);
+
+
+	}
+
+	public void BeginFade(int direction)
+	{
+		fadeDir = direction;
+	}
+
+	void OnLevelWasLoaded()
+	{
+		alpha = 1;
+		BeginFade (-1);
+	}
+}
---
+++ tower-game/Assets/Scripts/MenuButtonActions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonActions : MonoBehaviour {
+
+	public GameObject mainMenu;
+	public GameObject optionsMenu;
+
+
+	public void LoadSceneButton(string name)
+	{
+		StartCoroutine (ChangeScene (name, 1.5f));
+	}
+
+	public void mainToOptionsButton(float waitTime)
+	{
+		StartCoroutine (mainToOptions(waitTime));
+	}
+
+	public void optionsToMainButton(float waitTime)
+	{
+		StartCoroutine (optionsToMain(waitTime));
+	}
+
+	public void quitGame(float waitTime)
+	{
+		StartCoroutine (quitAfterTime (waitTime));
+	}
+
+
+
+
+	private IEnumerator ChangeScene(string SceneName, float waitTime)
+	{
+		yield return new WaitForSeconds (waitTime);
+		SceneManager.LoadScene (SceneName);
+	}
+
+	private IEnumerator mainToOptions(float waitTime)
+	{
+		yield return new WaitForSeconds (waitTime);
+		mainMenu.SetActive (false);
+		optionsMenu.SetActive (true);
+	}
+
+	private IEnumerator optionsToMain(float waitTime)
+	{
+		yield return new WaitForSeconds (waitTime);
+		optionsMenu.SetActive (false);
+		mainMenu.SetActive (true);
+	}
+
+	private IEnumerator quitAfterTime(float waitTime)
+	{
+		yield return new WaitForSeconds (waitTime);
+		Application.Quit ();
+		Debug.Log ("quit Game");
+	}
+}
